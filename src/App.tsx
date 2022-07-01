da4086115d9964e32ffcd213b13269fb5d0352ab
stanley <stanley.walkerv@gmail.com>
2022-07-01 16:17:17 -0400

    fixed face tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,39 @@
 import React, {
- Suspense, useMemo, useRef, useState,
+ Suspense, useRef,
 } from 'react';
-import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import {GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import {
   FaceTracker,
-  HeadMaskMesh,
   ZapparCamera,
   ZapparCanvas,
   Loader,
   Pipeline,
   Types,
-  FaceLandmark
 } from '@zappar/zappar-react-three-fiber';
 import { useLoader } from '@react-three/fiber'
 
-//import Scene from './Scene';
+import viniMaskBlack from './assets/viniMaskBlack.glb'
+import { showUI } from "@zappar/splash";
 
-
-import helmetSrc from './assets/updated/maskDebug.glb'
-//import helmetSrc from './assets/demoHelmet/z_helmet.glb'
 
 const Model = () => {
-  const gltf = useLoader(GLTFLoader, helmetSrc) as any ;
-   return <primitive scale={[.1, .1, .1]} position={[0.1, -.1, 0.1]} object={gltf.scene} />;
-  // return <primitive position={[0.25, -1.25, 0.1]} object={gltf.scene} />;
-  
+  const gltf = useLoader(GLTFLoader, viniMaskBlack) as any ;
+   return <primitive scale={[.13, .12, .12]} position={[0, 0, 0.5]} object={gltf.scene} />;
 };
+
+  // showUI({
+  //   // Hide the screen when user taps on the button.
+  //   onClick: (e) => {
+  //     // Pass 'false' to skip fading out.
+  //     e.destroy();
+  //   },
+  //   title: "My title",
+  //   subtitle: "My subtitle",
+  //   buttonText: "SUBMIT",
+  //   // background: "",
+  //   // logo: "",
+  // });
+
 
 function App() {
   const faceTrackerGroup = useRef<Types.FaceAnchorGroup>();
@@ -34,21 +42,14 @@ function App() {
     <ZapparCanvas>
       <ZapparCamera pipeline={pipeline} userFacing rearCameraMirrorMode="css" attachArray={undefined} attachObject={undefined} attachFns={undefined} />
       <FaceTracker ref={faceTrackerGroup} pipeline={pipeline} attachArray={undefined} attachObject={undefined} attachFns={undefined}>
-      {/* <FaceLandmark
-        target="nose-bridge"
-      > */}
         <Suspense fallback={null}>
           <Model />  
-
-          {/* <Scene> below is my fix to rendering the model, seems to work the same as <Model> */}
-          {/* <Scene/> */}
         </Suspense>
-        {/* </FaceLandmark> */}
       </FaceTracker>
       <ambientLight intensity={0.6} />
       <directionalLight position={[2.5, 8, 5]} intensity={4.5} />
-      {/* <Loader /> */}
-    </ZapparCanvas>
+      <Loader />
+    </ZapparCanvas> 
   );
 }
 
